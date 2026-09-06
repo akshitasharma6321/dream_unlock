003a9066792619826993472c1a8af267fe4f7815
akshitasharma6321 <akshitasharma6321@gmail.com>
2026-09-06 14:29:43 +0530

    Update logic.py

--- core/logic.py
+++ core/logic.py
@@ -6,14 +6,14 @@ def pee_review (submission):
     return 8.5
 def innovation_index (project):
     return 9.0
-def evaluate_talent(studnet):
+def evaluate_talent(student):
     score=0
-    score+=assess_skills(student['skills'])*0.40
-score+=peer_review(student['submission'])*0.30
-score+=innovation_index(student['project'])*0.30
+    score+=   assess_skills(student['skills'])*0.40
+   score+= peer_review(student['submission'])*0.30
+   score+= innovation_index(student['project'])*0.30
   if score>=8.5:
-return f"TOP PERFORMER->GLOBAL OPPORTUNITY (score:{score:.1f})"
-return f"CONTINUE=>New challenge assigned (score:{score:.1f})"
+  return f"TOP PERFORMER->GLOBAL OPPORTUNITY (score:{score:.1f})"
+  return f"CONTINUE=>New challenge assigned (score:{score:.1f})"
  
 #Test run 
 student={'skills':['Design',Commerce','coding'],
