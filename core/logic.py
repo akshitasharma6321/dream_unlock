adc142c23d05b013237f9e5356f6a510eda77a96
akshitasharma6321 <akshitasharma6321@gmail.com>
2026-09-06 14:40:26 +0530

    Update logic.py

--- core/logic.py
+++ core/logic.py
@@ -12,8 +12,8 @@ def evaluate_talent(student):
    score+= peer_review(student['submission'])*0.30
    score+= innovation_index(student['project'])*0.30
   if score>=8.5:
-  return f"TOP PERFORMER->GLOBAL OPPORTUNITY (score:{score:.1f})"
-  return f"CONTINUE=>New challenge assigned (score:{score:.1f})"
+  return f"TOP"
+  return f"CONTINUE"
  
 #Test run 
 student={'skills':['Design',Commerce','coding'],
