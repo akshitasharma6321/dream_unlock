f353bb0da9305338c0bfc765ea3d566641283a1b
akshitasharma6321 <akshitasharma6321@gmail.com>
2026-09-06 18:31:27 +0530

    Create main.dart

---
+++ core/main.dart
@@ -0,0 +1,24 @@
+import'package:flutter/material.dart';
+void main()=>runApp(A());
+class A extends StatelessWidget{
+build(c)=>MaterialApp(
+home: Scaffold(
+ appBar:AppBar
+(title:Text('Dream Unlock')),
+body :GridView.count(
+crossAxisCount:2,
+ children:[
+ Card(child:Center(
+ child:Text('Design'))),
+ Card(child:Center(
+ child:Text('commerce'))),
+ Card(child:Center(
+ child:Text('Medical'))),
+ Card(child:Center(
+ child:Text('Technology'))),
+ ]
+)
+)
+);
+}
+}
